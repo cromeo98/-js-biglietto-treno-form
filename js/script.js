38c3fb5a5ac5390bbb5ba521b481a7f029c80c10
cromeo98 <romeo.christian98@gmail.com>
2021-06-04 17:49:50 +0200

    add: inner html for user name

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@
 // Al click su "Annulla" dobbiamo ripulire il form e far sparire la sezione che calcola il prezzo del biglietto.
 // Bonus: aggiungete qualche controllo sui dati di input!
 // Buon lavoro e buon viaggio! :monorotaia:
+
+// 1) Creiamo un finto biglietto del treno con:
+
+// prima di tutto dichiaro una variabile per il bottono genera
+var genera = document.getElementById('genera')
+
+// aggiungo l'add event listener: gli dico cosa deve fare quando passo sopra la var "genera" (che corrisponde al bottone con id "genera" nell'HTML) e ci clicco sopra (click)
+
+genera.addEventListener("click", function(){
+
+// Nome passeggero
+// dichiaro una variabile nella quale salvare il valore dell'input tag con id="name"
+    var userName = document.getElementById('name').value;
+    console.log(userName);
+// inserisco il valore estratto all'interno del tag con id="user-name"
+    document.getElementById('user-name').innerHTML = userName
+ 
+})
+
+// Codice treno (numero casuale tra 90000 e 100000 escluso)
+// Numero carrozza (numero casuale tra 1 e 9 incluso)
+// Prezzo calcolato in base all'esercizio visto in precedenza
+// Categoria selezionata dall'utente
