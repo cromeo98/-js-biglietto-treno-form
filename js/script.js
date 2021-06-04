8a372009bc941707b9009ddfbecbc6c3345f92b3
cromeo98 <romeo.christian98@gmail.com>
2021-06-04 13:55:09 +0200

    add: basic html structure + add framework link

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Creiamo un layout come da screenshot, ma personalizzabile.
+// Creiamo un finto biglietto del treno con:
+// Nome passeggero
+// Codice treno (numero casuale tra 90000 e 100000 escluso)
+// Numero carrozza (numero casuale tra 1 e 9 incluso)
+// Prezzo calcolato in base all'esercizio visto in precedenza
+// Categoria selezionata dall'utente
+// Al click su "Genera" appare la schermata con le caratteristiche del biglietto.
+// Al click su "Annulla" dobbiamo ripulire il form e far sparire la sezione che calcola il prezzo del biglietto.
+// Bonus: aggiungete qualche controllo sui dati di input!
+// Buon lavoro e buon viaggio! :monorotaia:
